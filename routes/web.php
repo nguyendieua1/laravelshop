1835c83ad2b6f99a4b47faa74d52b60b4405fa49
nguyendieu <ctg.dieunx@gmail.com>
2022-08-01 07:35:45 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('admin/users/login',[LoginController::class,'index'])->name('login');
 Route::post('admin/users/login/store',[LoginController::class,'store']);
 
 Route::middleware(['auth'])->group(function (){
-    Route::get('dashboard',[MainController::class,'index'])->name('admin');
+    Route::get('admin/dashboard',[MainController::class,'index'])->name('admin');
     //Route::get('admin/main',[MainController::class,'index']);
 });
 
